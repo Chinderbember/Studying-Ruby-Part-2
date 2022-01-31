016aaf5fb8bf63f4db3e15a4f40f8b7d9daf7550
Chinderbember <chinderbember@yahoo.com>
2022-01-31 14:01:15 +0300

    changed task_5.rb

--- lesson 5/task_5/task_5.rb
+++ lesson 5/task_5/task_5.rb
@@ -3,14 +3,11 @@
 require_relative 'lib/class_list'
 
 list = List.new(*1..10)
-
-10.times do |n|
-  count = n + 1
-  puts "#{count} - й метод: #{list.send("instance#{count}")}"
-end
 list.each(*11..20)
 
 10.times do |n|
   count = n + 1
   puts "#{count} - й метод: #{list.send("instance#{count}")}"
 end
+
+
