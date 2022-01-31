db427d47266c145a318117525f737d70f1864ae6
Chinderbember <chinderbember@yahoo.com>
2022-01-31 10:38:01 +0300

    updated task_4.rb

--- lesson 5/task_4/lib/class_foo.rb
+++ lesson 5/task_4/lib/class_foo.rb
@@ -4,9 +4,9 @@ class Foo
   def initialize(methods = {})
     @methods = methods
     @methods.each do |method, value|
-      define_method method do 
+      define_singleton_method method do
         value
       end
     end
-  end 
+  end
 end
--- lesson 5/task_4/task_4.rb
+++ lesson 5/task_4/task_4.rb
@@ -3,13 +3,11 @@
 require_relative 'lib/class_foo'
 
 hash = {
-		say_hello: 'Hello',
-		say_ruby: 'Ruby'
+  say_hello: 'Hello',
+  say_ruby: 'Ruby'
 }
 
 obj = Foo.new(hash)
 
 puts obj.say_hello
 puts obj.say_ruby
-
-puts obj.methods
