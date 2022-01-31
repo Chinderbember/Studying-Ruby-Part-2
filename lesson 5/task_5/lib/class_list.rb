97ea752579e901d0669678c284e05c7b5000c599
Chinderbember <chinderbember@yahoo.com>
2022-01-31 13:25:49 +0300

    updated task_5

--- lesson 5/task_5/lib/class_list.rb
+++ lesson 5/task_5/lib/class_list.rb
@@ -1,14 +1,26 @@
 # frozen_string_literal: true
 
+# rubocop:disable Style/ClassVars
 class List
+  class << self
+    def count
+      @@count
+    end
+
+    def increase_count(number)
+      @@count += number
+    end
+  end
   def initialize(*params)
-    @count = 1
+    @@count = 1
+    obj = self
     params.each do |element|
-      class << self
-        attr_accessor "instance#{@count}".to_sym
+      class << obj
+        attr_accessor("instance#{List.count}".to_sym)
       end
-      self.send("instance#{@count}=", element)
-      @count += 1
+      obj.send("instance#{List.count}=".to_sym, element)
+      List.increase_count(1)
     end
   end
 end
+# rubocop:enable Style/ClassVars
--- lesson 5/task_5/task_5.rb
+++ lesson 5/task_5/task_5.rb
@@ -4,4 +4,7 @@ require_relative 'lib/class_list'
 
 list = List.new(*1..10)
 
-puts list.instance2
+10.times do |n|
+  count = n + 1
+  puts "#{count} - й метод: #{list.send("instance#{count}")}"
+end
