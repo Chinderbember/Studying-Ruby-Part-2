349c2b4aabd3f9ad1a336f084520a65aa9e5c4b4
Chinderbember <chinderbember@yahoo.com>
2022-02-03 22:12:44 +0300

    stylistic correction task_1.rb

--- lesson 6/task_1/lib/array_processing_class.rb
+++ lesson 6/task_1/lib/array_processing_class.rb
@@ -1,9 +1,11 @@
-class Array_processing
-	def self.arr_for_hash(arr_fst, arr_snd)
-		result_arr = []
-		arr_fst.size.times do |n|
-			result_arr << [arr_fst[n], arr_snd[n]]
-		end
-		result_arr
+# frozen_string_literal: true
+
+class ArrayProcessing
+  def self.arr_for_hash(arr_fst, arr_snd)
+    result_arr = []
+    arr_fst.size.times do |n|
+      result_arr << [arr_fst[n], arr_snd[n]]
+    end
+    result_arr
   end
 end
--- lesson 6/task_1/task_1.rb
+++ lesson 6/task_1/task_1.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
+
 require_relative 'lib/array_processing_class'
 
 arr_fst = %i[first second third]
 arr_snd = [*1..arr_fst.size]
 
-puts Array_processing.arr_for_hash(arr_fst, arr_snd ).to_h
+puts ArrayProcessing.arr_for_hash(arr_fst, arr_snd).to_h
