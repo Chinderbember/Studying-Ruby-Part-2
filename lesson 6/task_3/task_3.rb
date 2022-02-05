81373a0df841e0edd9864a679870cc811fcb6298
Chinderbember <chinderbember@yahoo.com>
2022-02-05 16:55:34 +0300

    stylistic correction in task_3

--- lesson 6/task_3/task_3.rb
+++ lesson 6/task_3/task_3.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
+str = <<~HERE
+  Возьмите текст этого задания и извлеките из него все слова, количество символов в которых
+  больше 5. Подсчитайте количество слов и выведите их алфавитном порядке.
+HERE
 
-str = <<here
-Возьмите текст этого задания и извлеките из него все слова, количество символов в которых
-больше 5. Подсчитайте количество слов и выведите их алфавитном порядке.
-here
-
-arr = (str.downcase.split.map { |str| str.chomp('.').chomp(',') }).select {|str| str.size > 5}
+arr = (str.downcase.split.map { |string| string.chomp('.').chomp(',') }).select { |string| string.size > 5 }
 puts "Количество слов, в которых символов больше 5: #{arr.size}"
-puts arr.sort 
+puts arr.sort
