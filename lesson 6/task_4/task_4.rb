281475fd7530359035e550c0827abb656ba214d2
Chinderbember <chinderbember@yahoo.com>
2022-02-05 17:41:54 +0300

    stylistic correction lesson 6 task_4

--- lesson 6/task_4/task_4.rb
+++ lesson 6/task_4/task_4.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 str = <<~HERE
-  Возьмите текст этого задания и извлеките из него все слова, которые начинаются с символа
-  'и'. Сформируйте из них список уникальных слов и выведите их в порядке увеличения
-	количества символов в слове.
+   Возьмите текст этого задания и извлеките из него все слова, которые начинаются с символа
+   'и'. Сформируйте из них список уникальных слов и выведите их в порядке увеличения
+  количества символов в слове.
 HERE
 
-prep_arr = str.downcase.split.map { |string| string.chomp('.').chomp(',') }
-final_arr = (prep_arr.select { |string| string[0] == 'и' }).uniq.sort_by { |str| str.size} 
 
+prep_arr_fst = str.downcase.split.map { |string| string.chomp('.').chomp(',') }
+prep_arr_snd = prep_arr_fst.select { |string| string[0] == 'и' }
+final_arr = prep_arr_snd.uniq.sort_by(&:size)
 puts final_arr
