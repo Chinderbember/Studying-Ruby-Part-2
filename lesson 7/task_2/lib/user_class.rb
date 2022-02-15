7f0d972a088b01a3c8a40b405dc78a9d3d94bb31
Chinderbember <chinderbember@yahoo.com>
2022-02-15 15:36:51 +0300

    added lesson 7 task_2

---
+++ lesson 7/task_2/lib/user_class.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class User
+  attr_accessor :surname, :name, :patronymic, :email
+
+  def initialize 
+    yield self
+  end
+
+end
---
+++ lesson 7/task_2/task_2.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative 'lib/user_class'
+
+default_user = User.new do |user|
+	user.surname = 'default surname'
+	user.name = 'default name'
+	user.patronymic = 'default patronymic'
+	user.email = 'default email'
+end
+
+puts default_user.surname
+puts default_user.name 
+puts default_user.patronymic
+puts default_user.email
