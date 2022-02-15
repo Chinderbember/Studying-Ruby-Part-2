268d7ee058506755454990808f0f08ffbb2cfaad
Chinderbember <chinderbember@yahoo.com>
2022-02-15 15:20:01 +0300

    refactoring lesson 7 task_1

--- lesson 7/task_1/task_1.rb
+++ lesson 7/task_1/task_1.rb
@@ -1,27 +1,5 @@
 # frozen_strinf_literal: true
-
-class Integer
-	BYTES_IN_KILOBYTES = 1024
-	BYTES_IN_MEGABYTES = BYTES_IN_KILOBYTES * 1024
-	BYTES_IN_GIGABYTES = BYTES_IN_MEGABYTES * 1024
-	BYTES_IN_TERABYTES = BYTES_IN_GIGABYTES * 1024
-
-	def kilobytes
-		self * BYTES_IN_KILOBYTES
-	end
-
-	def megabytes
-		self * BYTES_IN_MEGABYTES
-	end
-
-	def gigabytes
-		self * BYTES_IN_GIGABYTES
-	end
-
-	def terabytes
-		self * BYTES_IN_TERABYTES
-	end
-end
+require_relative "lib/reworked_integer"
 
 puts 5.kilobytes
 puts 5.megabytes
