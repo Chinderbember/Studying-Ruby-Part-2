ab4f03b6982ca84907004b994547c0006c3ff642
Chinderbember <chinderbember@yahoo.com>
2022-02-15 15:24:28 +0300

    task_1 lesson 7 stylistic corrections

--- lesson 7/task_1/lib/reworked_integer.rb
+++ lesson 7/task_1/lib/reworked_integer.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 class Integer
-	BYTES_IN_KILOBYTES = 1024
-	BYTES_IN_MEGABYTES = BYTES_IN_KILOBYTES * 1024
-	BYTES_IN_GIGABYTES = BYTES_IN_MEGABYTES * 1024
-	BYTES_IN_TERABYTES = BYTES_IN_GIGABYTES * 1024
+  BYTES_IN_KILOBYTES = 1024
+  BYTES_IN_MEGABYTES = BYTES_IN_KILOBYTES * 1024
+  BYTES_IN_GIGABYTES = BYTES_IN_MEGABYTES * 1024
+  BYTES_IN_TERABYTES = BYTES_IN_GIGABYTES * 1024
 
-	def kilobytes
-		self * BYTES_IN_KILOBYTES
-	end
+  def kilobytes
+    self * BYTES_IN_KILOBYTES
+  end
 
-	def megabytes
-		self * BYTES_IN_MEGABYTES
-	end
+  def megabytes
+    self * BYTES_IN_MEGABYTES
+  end
 
-	def gigabytes
-		self * BYTES_IN_GIGABYTES
-	end
+  def gigabytes
+    self * BYTES_IN_GIGABYTES
+  end
 
-	def terabytes
-		self * BYTES_IN_TERABYTES
-	end
+  def terabytes
+    self * BYTES_IN_TERABYTES
+  end
 end
--- lesson 7/task_1/task_1.rb
+++ lesson 7/task_1/task_1.rb
@@ -1,8 +1,8 @@
-# frozen_strinf_literal: true
-require_relative "lib/reworked_integer"
+# frozen_string_literal: true
+
+require_relative 'lib/reworked_integer'
 
 puts 5.kilobytes
 puts 5.megabytes
 puts 5.gigabytes
 puts 5.terabytes
-
