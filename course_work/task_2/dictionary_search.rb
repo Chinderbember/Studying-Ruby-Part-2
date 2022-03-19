6d2bdc72b396425af0395fb9953173095ab4f7e8
Chinderbember <chinderbember@yahoo.com>
2022-03-19 20:35:53 +0300

    refactoring 2

--- course_work/task_2/dictionary_search.rb
+++ course_work/task_2/dictionary_search.rb
@@ -2,21 +2,20 @@
 
 require_relative 'lib/dictionary'
 
-
 str = ARGV[0]
-# Присвоение кажется бесполезным, но это нет так. Не знаю почему, но когда я пытался оставить здесь лишь вызов метода puts Dictionary.search(ARGV[0], 'lib/dictionary.txt')
-# , то в консоли происходило приведённое ниже. Может это баг интерпретатора или я что-то не знаю. Надеюсь на ваш поясняющий комментарий.
-# 
+# rubocop:disable Layout/LineLength
+# Присвоение кажется бесполезным, но это нет так. Не знаю почему, но когда я пытался оставить здесь лишь вызов метода
+# puts Dictionary.search(ARGV[0], 'lib/dictionary.txt'), то в консоли происходило приведённое ниже.
+# Может это баг интерпретатора или я что-то не знаю. Надеюсь на ваш поясняющий комментарий.
+#
 # D:\Документы\coding\Ruby Basics part 2\practical exercises\course_work\task_2>ruby dictionary_search.rb слово
 # D:/Документы/coding/Ruby Basics part 2/practical exercises/course_work/task_2/lib/dictionary.rb:7:in `gets': No such file or directory @ rb_sysopen - слово (Errno::ENOENT)
 #         from D:/Документы/coding/Ruby Basics part 2/practical exercises/course_work/task_2/lib/dictionary.rb:7:in `gets'
 #         from D:/Документы/coding/Ruby Basics part 2/practical exercises/course_work/task_2/lib/dictionary.rb:7:in `open_dict'
 #         from D:/Документы/coding/Ruby Basics part 2/practical exercises/course_work/task_2/lib/dictionary.rb:12:in `search'
 #         from dictionary_search.rb:5:in `<main>'
+# rubocop:enable Layout/LineLength
 
 ARGV.pop
 
 puts Dictionary.search(str, 'lib/dictionary.txt')
-
-
-
--- course_work/task_2/lib/dictionary.rb
+++ course_work/task_2/lib/dictionary.rb
@@ -4,34 +4,33 @@ class Dictionary
   class << self
     def open_dict(file)
       $stdin = File.open(file)
-      dict_in_str = gets("$$") 
+      dict_in_str = gets('$$')
       dict_in_str.split
     end
 
-    def search(word, file)
+    def search(word, file, new_dict = [])
       dict = open_dict(file)
-      new_dict = []
-
       found = nil
       dict.each do |word_in_dict|
         if word_in_dict == word
           found = true
           break
-        elsif word_in_dict.index(word) == 0
+        elsif word_in_dict.index(word).zero?
           new_dict << word_in_dict
         end
       end
+      search_result(found, word, new_dict)
+    end
 
+    def search_result(found, word, new_dict)
       if found
         word
-      elsif new_dict.empty? 
+      elsif new_dict.empty?
         'no elements'
-      else 
+      else
         new_dict
       end
-
     end
+    private :open_dict, :search_result
   end
 end
-
-
