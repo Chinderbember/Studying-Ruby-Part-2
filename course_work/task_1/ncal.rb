7771a352dc257418e23f4d7986aa443a279077b8
Chinderbember <chinderbember@yahoo.com>
2022-03-19 16:03:57 +0300

    refactoring task_1 of course work

--- course_work/task_1/ncal.rb
+++ course_work/task_1/ncal.rb
@@ -1,26 +1,5 @@
-require 'date'
+# frozen_string_literal: true
 
-SIZE = 3
-WEEK_DAYS = %w[пн вт ср чт пт сб вс]
-def this_month_cal
-	today = Time.now.to_date
-	today_month = today.mon 
-	today_year = today.year
-	cweek_fst_day = Date.new(today_year, today_month, 1).cweek
-	1.upto(7) do |wday|
-		print WEEK_DAYS[wday - 1]
-		output_day = Date.commercial(today_year, cweek_fst_day, wday)
-		loop do 
-			if output_day.mon != today_month
-				print " " * SIZE
-			else
-				print format("% #{SIZE}d", output_day.mday)
-			end
-			output_day += 7
-			break if output_day.mon != today_month
-		end
-		puts
-	end
-end
+require_relative 'lib/calendar'
 
-this_month_cal
+Calendar.this_month
